84a858235f4e9c70df8d5e7fa5d17c3a248a77a8
krishnazure <krishnaazure2014@gmail.com>
2015-11-04 06:17:40 -0500

    The Script Adds User Route
    Script Adds User Route for network traffic. For example to push all the
    network traffic from VM to the Frontend(Barracuda NG Firewall)

---
+++ AddUserRoute.ps1
@@ -0,0 +1,24 @@
+﻿
+Switch-AzureMode -Name AzureServiceManagement 
+
+$SubscriptionName = "Internal Consumption"
+$Location = "East US"
+$NGIPaddress = "10.0.0.5"
+$vnetnametest = "AzureVNET"
+$Subnetname = "Frontend"
+$ngservicetest= "BNG-MultiNIC"
+
+Select-AzureSubscription -SubscriptionName $SubscriptionName –Current
+
+
+#Create  a new routetable 
+New-AzureRouteTable -Name NGRouteTest -Location $location -Label "Route to NG Firewall"
+ 
+#Routes to Route table 
+Get-AzureRouteTable -Name NGRouteTest | Set-AzureRoute -RouteName Default -AddressPrefix 0.0.0.0/0 -NextHopType VirtualAppliance -NextHopIpAddress $NGIPaddress
+ 
+#Bind Routetable to Subnet 
+Set-AzureSubnetRouteTable -VirtualNetworkName $vnetnametest -SubnetName $Subnetname -RouteTableName NGRouteTest 
+ 
+#IpForwarding 
+get-azurevm -ServiceName $ngservicetest -Name $vnnametest | Set-AzureIPForwarding -Enable
